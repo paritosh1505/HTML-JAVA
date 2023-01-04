7bdad0c54754b4a76fd7f6817fddae5f8ad8edc5
mahima2708 <mhtamta5@gmail.com>
2023-01-04 15:32:51 +0530

    new commit bytag

---
+++ inlinesrc.js
@@ -0,0 +1,8 @@
+//getelementsbyclassname//
+var li= document.getElementsByClassName('listitem');
+li[0].style.backgroundColor="green";
+
+//getelementbytagname//
+
+var it = document.getElementsByTagName('li');
+it[4].style.fontWeight='bold';
