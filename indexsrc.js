cb3636ac082fede9faedacbd63ee70e3ef00ef65
mahima2708 <mhtamta5@gmail.com>
2023-01-04 11:36:12 +0530

    element by class

---
+++ indexsrc.js
@@ -0,0 +1,8 @@
+var items = document.getElementsByClassName('list-group-item');
+items[2].style.backgroundColor='green';
+
+for(var i=0;i<items.length;i++)
+{
+    items[i].style.fontWeight='bold';
+    items[i].style.color= 'blue';
+}
