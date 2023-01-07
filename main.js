9e8eaff0a65d972243b79f3e68ca40076311c5b4
mahima2708 <mhtamta5@gmail.com>
2023-01-07 17:37:25 +0530

    storing data

---
+++ main.js
@@ -0,0 +1,30 @@
+const myform = document.querySelector('#my-form');
+const nameInput = document.querySelector('#name');
+const emailInput= document.querySelector('#email');
+const phone_number=document.querySelector('#phnumber');
+const msg= document.querySelector('.msg');
+const userList = document.querySelector('#users');
+myform.addEventListener('submit', onSubmit);
+function onSubmit(e){
+    e.preventDefault();
+    console.log(nameInput.value);
+    if(nameInput.value ==='' || emailInput.value ==='' || phone_number.value==='')
+    {
+        msg.classList.add('error');
+        msg.innerHTML='PLEASE ENTER ALL FIELDS';
+        setTimeout(()=>msg.remove() , 3000);
+
+    }
+    else{
+        const li = document.createElement('li');
+        localStorage.setItem('nameInput', nameInput.value);
+        localStorage.setItem('email', emailInput.value);
+        localStorage.setItem('phone number',phone_number.value);
+
+    li.appendChild(document.createTextNode(`${nameInput.value}: ${emailInput.value}: ${phone_number.value}`));
+    userList.appendChild(li);
+    nameInput.value = '';
+    emailInput.value = '';
+    phone_number.value = '';
+  }
+}
